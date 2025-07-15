0ea77fe560b2b802efd3491c29589c3c0c1f4711
otoneumann <otarshenge@gmail.com>
2025-07-15 20:53:48 +0200

    Form

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,17 +2,46 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\ProductRepository;
+use App\Form\ProductType;
+use Symfony\Component\HttpFoundation\Request;
 
 final class ProductController extends AbstractController
 {
     #[Route('/products', name: 'product_index')]
-    public function index(): Response
+    public function index(ProductRepository $repository): Response
     {
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+            'products' => $repository->findAll()
         ]);
     }
+
+    #[Route('/product/{id<\d+>}', name: 'product_show')]
+    public function show(Product $product): Response
+    {
+        return $this->render('product/show.html.twig', [
+            'product' => $product
+        ]);
+
+    }
+
+    #[Route('/product/new', name: 'product_new')]
+    public function new(Request $request): Response
+    {
+        $form = $this->createForm(ProductType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            dd($request->request->all());
+        }
+        return $this->render('product/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
 }
